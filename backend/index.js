c43517993eef91ed6fff448201e0adf9e467bf2b
single-byte <joharkhan1999@gmail.com>
2024-01-19 23:04:23 +0500

    Add body-parser dependency and remove unused code

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from "express";
 import dontenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
 import connectToDB from "./config/db.js";
+import MainRouter from "./routes/MainRouter.js";
 dontenv.config();
 
 const app = express();
-const PORT = process.env.SERVER_PORT || 8080;
 
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api", MainRouter);
+
+const PORT = process.env.SERVER_PORT || 8080;
 const sequelize = await connectToDB();
 if (sequelize) {
   app.listen(PORT, () => {
